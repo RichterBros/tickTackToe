6a2d654941a399b9396e8fa2ffb15108c14c5250
Erich Richter and Joseph Wangemann <joseph@>
2020-01-16 16:38:53 -0800

    create initial files

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+
+
+var turn = 0;
+
+$(document).ready(function() {
+  
+  
+  
+  $("#1a").click(function() {
+    $("#1a").text("X");
+    turn++;
+    console.log(turn);
+  });
+    
+  $("#1b").click(function() {
+    $("#1b").text("X");
+  
+  });
+   
+  $("#1c").click(function() {
+    $("#1c").text("X");
+  
+  });
+
+
+}); 
